358806224a0c65edc9cae446114579cdf60834a8
Ayushhgupta39 <ayushgupta3902@gmail.com>
2023-04-11 11:02:47 +0530

    Assignment Complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,20 @@ function App() {
   return (
     <div className="App">
       <Box display={"flex"} alignItems={"center"} justifyContent={"center"} width={"100%"} height={"100vh"}>
-        <Card maxW={"64"} borderRadius={"2xl"} maxH={"md"}>
-          <CardHeader p={"2"} alignContent={"center"} justifyContent={"center"}>
+        <Card boxShadow={"lg"} maxW={"64"} borderRadius={"2xl"} maxH={"md"}>
+          <CardHeader p={"3 "} alignContent={"center"} justifyContent={"center"}>
             <Box>
               <img className='qr_image' src="/image-qr-code.png" alt="image_description" width="image_width" height="image_height" />
             </Box>
           </CardHeader>
-          <CardBody>
-            <Box>
-              <Text>
+          <CardBody p={"3"}>
+            <Box className='qr_content_heading'>
+              <Text fontSize={"large"} textAlign={"center"}>
                 Improve your front-end skills by building projects
               </Text>
             </Box>
-            <Box>
-              <Text>
+            <Box m={"2"} className='qr_content'>
+              <Text textColor={"gray.500"} textAlign={"center"} fontSize={"smaller"}>
                 Scan the QR code to visit Frontend Mentor and take your coding skills to the next level.
               </Text>
             </Box>
